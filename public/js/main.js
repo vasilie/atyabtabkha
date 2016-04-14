270e82298326bdd838dc519d59cab690ea789883
diwanee <diwanee@Mac-Lutalica.local>
2016-04-14 17:33:29 +0200

    article

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,10 @@ $(function(){
     $(".header__menu-ico").toggleClass("active");
   });
 });
+$(window).resize(function(){
+  if ($(window).width()<997){
+    var percent =  $(window).width() /997;
+    var left = (100 - (100 * percent)) / 2;
+    $(".mpu").css({"transform":"scale("+percent+")"});
+  }
+});
